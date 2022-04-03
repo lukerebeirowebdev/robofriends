3915d8b5a770ff23e8f5e3f8daf9ba6fa33d3082
Luke Rebiro <68423198+lukerebeirowebdev@users.noreply.github.com>
2022-04-03 15:22:54 +0100

    added scrolling component to card window

---
+++ src/Scroll.js
@@ -0,0 +1,14 @@
+// added scrolling functionality to the component
+
+import React from 'react';
+
+const Scroll = (props) => {
+    return (
+        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px' }}>
+            {props.children}
+        </div>
+    );
+};
+
+
+export default Scroll;
