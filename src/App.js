4c237d9d452978cd03bbc6197c68b69dcb12f9e5
Luke Rebiro <68423198+lukerebeirowebdev@users.noreply.github.com>
2022-04-03 15:22:16 +0100

    added font, and new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css';
 
 
 
@@ -9,23 +10,42 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
-    onSearchChange(event) {
-        console.log(event);
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
     }
 
     render() {
-        return (
-            <div className="tc">
-                <h1> RoboFriends </h1>
-                <SearchBox searchChanged={this.onSearchChange} />
-                <CardList robots={this.state.robots} />
-            </div>
-        );
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
+
+                </div>
+            );
+        }
+
     }
 }
 
