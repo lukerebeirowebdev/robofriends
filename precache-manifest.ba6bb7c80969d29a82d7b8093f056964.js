193e0ef81db78d5fd6431b0d1709d1c324e46f6d
Luke Rebiro <68423198+lukerebeirowebdev@users.noreply.github.com>
2022-04-12 16:20:26 +0100

    Updates

---
+++ precache-manifest.ba6bb7c80969d29a82d7b8093f056964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b85945988f6680123ed",
+    "url": "/robofriends/static/css/main.e2e66943.chunk.css"
+  },
+  {
+    "revision": "5b85945988f6680123ed",
+    "url": "/robofriends/static/js/main.5b859459.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/robofriends/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "413ffdedc5c97a62fd2c",
+    "url": "/robofriends/static/css/2.d490b25a.chunk.css"
+  },
+  {
+    "revision": "413ffdedc5c97a62fd2c",
+    "url": "/robofriends/static/js/2.413ffded.chunk.js"
+  },
+  {
+    "revision": "cf2212719657acf7eaba",
+    "url": "/robofriends/static/js/runtime~main.cf221271.js"
+  },
+  {
+    "revision": "c16ca397ca36028f946f828e788ad204",
+    "url": "/robofriends/static/media/robotaur.c16ca397.woff"
+  },
+  {
+    "revision": "3b71546b830b85e1bfdd2d0ae31c095c",
+    "url": "/robofriends/index.html"
+  }
+];
